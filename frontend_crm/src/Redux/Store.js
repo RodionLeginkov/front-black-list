aec46b8abbbad077c1603378f9d49cf695a83a08
NikitaStepanenko <stepanenkonikit@gmail.com>
2020-03-25 10:13:17 +0300

    apdate userInvitePage

--- frontend_crm/src/Redux/Store.js
+++ frontend_crm/src/Redux/Store.js
@@ -5,7 +5,7 @@ import rootReducers from './Reducers/index';
 const store = createStore(rootReducers, compose(
   applyMiddleware(thunk),
   // eslint-disable-next-line no-underscore-dangle
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 
 ));
 

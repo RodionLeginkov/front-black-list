9bb7558cfaf4627c546ac8b83f751a2eddd86e2c
Rodion <rodion_leginkov@mail.ru>
2020-03-31 18:15:39 +0300

    heroku fix

--- frontend_crm/src/Redux/Actions/MilestonesActions/MilestonesApi.js
+++ frontend_crm/src/Redux/Actions/MilestonesActions/MilestonesApi.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 async function postData(url, data) {
-    const response = await axios.post(url, data)
-    return response;
+  const response = await axios.post(url, data);
+  return response;
 }
 
-export const postMilestone = (data) => postData(`${process.env.REACT_APP_BASE_API}/milestones`, data)
+export const postMilestone = (data) => postData(`${process.env.REACT_APP_BASE_API}milestones`, data);

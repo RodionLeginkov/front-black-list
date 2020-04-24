4333d40a5b1d2dcca755b2fb661691a91674c66e
NikitaStepanenko <stepanenkonikit@gmail.com>
2020-04-24 19:34:46 +0300

    Fix heroku bag

--- frontend_crm/src/components/MilestoneInfoModal/MilestoneInfoModal.jsx
+++ frontend_crm/src/components/MilestoneInfoModal/MilestoneInfoModal.jsx
@@ -73,7 +73,8 @@ const MilestoneInfoModal = (props) => {
     e.preventDefault();
     setOpenModal(false);
   };
-  const paymentType = `${project.rate_type !== '' ? paymentTypes.find((item) => item.value === project.rate_type).label : '–'}`;
+  let paymentType = `${project.rate_type !== '' ? paymentTypes.find((item) => item.value === project.rate_type) : '–'}`;
+  if (paymentType.label !== undefined) paymentType = paymentType.label;
   // const paymentType = paymentTypes.find((item) => item.value === project.rate_type).label;
   let createDate = new Date(project.start_date);
   let endDate = new Date(project.end_date);
